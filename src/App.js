3f15a7c88f4b178859952117796437efceac2c45
Justen Schilinski <justenschilinski34@hotmail.com>
2023-04-08 21:36:45 -0400

    Added react bootstrap and started work on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Skills</Nav.Link>
+            <Nav.Link href="#home">Projects</Nav.Link>
+          </Nav>
+        </Navbar>
       </header>
     </div>
   );
