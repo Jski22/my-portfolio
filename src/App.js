2bd3c5f0ed02946a51bef676de23dd47a904b548
Justen Schilinski <justenschilinski34@hotmail.com>
2023-04-09 22:27:38 -0400

    Added about me link to navbar and changed position of nav elements

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Skills</Nav.Link>
-            <Nav.Link href="#home">Projects</Nav.Link>
+          <Navbar.Brand href="#home" className="ms-4">React-Bootstrap</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="pe-4">About Me</Nav.Link>
+            <Nav.Link href="#home" className="pe-4">Skills</Nav.Link>
+            <Nav.Link href="#home" className="pe-4">Projects</Nav.Link>
           </Nav>
         </Navbar>
       </header>
