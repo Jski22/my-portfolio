9423b05b285696482dc368afb3903be009e998d3
Justen Schilinski <justenschilinski34@hotmail.com>
2023-04-10 22:16:30 -0400

    Changed navbar brand and added initial layout for about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,14 +11,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="#home" className="ms-4">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" className="ms-4">Justen Schilinski</Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className="pe-4">About Me</Nav.Link>
+            <Nav.Link href="#home" className="pe-4">About</Nav.Link>
             <Nav.Link href="#home" className="pe-4">Skills</Nav.Link>
             <Nav.Link href="#home" className="pe-4">Projects</Nav.Link>
           </Nav>
         </Navbar>
       </header>
+      <body>
+        <Container id="About">
+          <Row>
+            <Col lg={8}>
+              <Container id="profile-picture" className="align-self-center"></Container>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </body>
     </div>
   );
 }
